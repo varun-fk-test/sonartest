f498c2c8582851c21b36f2c6361dbd2df9cb1dc2
ekao <eric.kao@fourkites.com>
2021-07-19 15:23:24 -0700

    add parameters

--- sonarservice.groovy
+++ sonarservice.groovy
@@ -4,6 +4,9 @@
 stage('Sonar Code Analysis'){
   sonarCodeAnalysisTesting(
     service: "$SERVICE",
+    sonarprojectkey: "$SONARPROJECTKEY",
+    sonarhosturl: "$SONARHOSTURL",
+    sonarlogin: "$SONARLOGIN",
     giturl: "$GITURL",
     branch: "$GITBRANCH"
   )
